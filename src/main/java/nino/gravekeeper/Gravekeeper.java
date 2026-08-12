5c70eb4bfd5568f6b154946370452ecb117cba7a
NinoTheNoob <NinoTheNoob@users.noreply.github.com>
2026-08-12 19:33:57 +0200

    wire everything together in the main plugin class

---
+++ src/main/java/nino/gravekeeper/Gravekeeper.java
@@ -0,0 +1,45 @@
+package nino.gravekeeper;
+
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import nino.gravekeeper.command.GraveCommand;
+import nino.gravekeeper.command.GravekeeperAdminCommand;
+import nino.gravekeeper.listener.ChunkRehydrateListener;
+import nino.gravekeeper.listener.DeathListener;
+import nino.gravekeeper.listener.GraveInteractListener;
+import nino.gravekeeper.manager.GraveManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Gravekeeper extends JavaPlugin {
+
+    private GraveManager graveManager;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        graveManager = new GraveManager(this);
+        graveManager.loadAll();
+
+        getServer().getPluginManager().registerEvents(new DeathListener(graveManager), this);
+        getServer().getPluginManager().registerEvents(new GraveInteractListener(this, graveManager), this);
+        getServer().getPluginManager().registerEvents(new ChunkRehydrateListener(graveManager), this);
+
+        GraveCommand graveCommand = new GraveCommand(this, graveManager);
+        GravekeeperAdminCommand adminCommand = new GravekeeperAdminCommand(this);
+        getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, event -> {
+            graveCommand.register(event.registrar());
+            adminCommand.register(event.registrar());
+        });
+
+        getLogger().info("Gravekeeper has been enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Gravekeeper has been disabled.");
+    }
+
+    public GraveManager getGraveManager() {
+        return graveManager;
+    }
+}
